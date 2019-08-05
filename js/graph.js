7022ed879cfbb5a5a05fc273f079838c8b9b8048
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-08-05 14:48:04 +0200

    Removed graph2.js and graph3.js, copied the code from graph3.js into graph.js.

--- js/graph.js
+++ js/graph.js
@@ -1,16 +1,46 @@
 ;(function() {
-  const data = [0, 30, 10, 50, 20, 50, 120]
+  const graph = document.getElementById("graph-1")
+  const DIMENSIONS = svgDimensions(graph)
+  const DATA = randomData(10)
 
-  const graph = document.querySelector("#graph-1 svg")
+  // Probably shouldn't be a global
+  let convert = pointsConverter({
+    dimensions: DIMENSIONS,
+    padding: [10, 10],
+    data: DATA
+  })
 
-  function svgDimensions(svg) {
-    const css = getComputedStyle(svg)
+  function init() {
+    const path = createPath(DATA, {
+      stroke: "blue",
+      "stroke-width": 2,
+      fill: "none"
+    })
 
-    return {
-      width: Number(css.width.replace("px", "")),
-      height: Number(css.height.replace("px", ""))
-    }
+    path.setAttribute("stroke", "green")
+
+    graph.appendChild(path)
   }
+
+  init()
+
+  function randomData(length) {
+    const data = []
+
+    while (data.length < length) {
+      data.push([data.length, 10 * Math.round(10 * Math.random())])
+    }
+
+    return data
+  }
+
+  // setInterval(() => {
+  //   DATA.shift()
+
+  //   DATA.push(randomData(10))
+
+  //   pathTransition(DATA[0], DATA[1], 30)
+  // }, 3000)
 
   function applyAttributes(target, attributes) {
     for (let attr in attributes) {
@@ -18,294 +48,152 @@
     }
   }
 
-  function roundNicely(num, exp) {
-    return Math.ceil(num / exp) * exp
+  function svgDimensions(svg) {
+    const css = getComputedStyle(svg)
+
+    return [
+      Number(css.width.replace("px", "")),
+      Number(css.height.replace("px", ""))
+    ]
   }
 
-  function xAxis(props) {
-    const {
-      xRange,
-      yRange,
-      color = "blue",
-      thickness,
-      text = false,
-      font,
-      data
-    } = props
+  function pointsConverter(params) {
+    const { dimensions, padding, data } = params
 
-    const container = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "g"
-    )
+    createRect(DIMENSIONS, padding)
 
-    const horizontalLine = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "line"
-    )
+    const verticalPadding =
+      padding.length > 2 ? padding[0] + padding[2] : padding[0] * 2
+    const horizontalPadding =
+      padding.length > 2 ? padding[1] + padding[3] : padding[1] * 2
 
-    // Set the coordinates for the horizontal line
-    applyAttributes(horizontalLine, {
-      x1: xRange[0],
-      x2: xRange[1],
-      y1: yRange[1],
-      y2: yRange[1]
-    })
+    const width = dimensions[0] - horizontalPadding
+    const height = dimensions[1] - verticalPadding
 
-    // Set the styling for the horizontal line
-    applyAttributes(horizontalLine, {
-      stroke: typeof color == "object" ? color[0] : color,
-      "stroke-width": thickness[0] || thickness || 1
-    })
+    const yData = data.map(d => d[1])
 
-    // Add horizontal line to the container
-    container.appendChild(horizontalLine)
+    const min = 0
+    const max = Math.max(...yData)
 
-    // Create the vertical lines
-    for (let i = 0; i <= data.length - 1; i++) {
-      const group = document.createElementNS("http://www.w3.org/2000/svg", "g")
+    const yRatio = height / (max - min)
+    const xRatio = width / (data.length - 1)
 
-      const verticalLine = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "line"
-      )
-
-      const xStep = (xRange[1] - xRange[0]) / (data.length - 1)
-
-      // Set the coordinates for the vertical line
-      applyAttributes(verticalLine, {
-        x1: xRange[0] + i * xStep,
-        x2: xRange[0] + i * xStep,
-        y1: yRange[0],
-        y2: yRange[1]
-      })
-
-      // Set the styling for the vertical line
-      applyAttributes(verticalLine, {
-        stroke: typeof color == "object" ? color[1] || color[0] : color,
-        "stroke-width": thickness[1] || thickness || 1
-      })
-
-      // Add vertical line to the group
-      group.appendChild(verticalLine)
-
-      // Add text if specified
-      if (text) {
-        const label = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "text"
-        )
-
-        if (text === true) {
-          // Text is true use i as text
-          const text = document.createTextNode(i)
-
-          label.appendChild(text)
-        } else if (typeof text === "string") {
-          // Use the string as key for the data
-        }
-
-        // Set the coordinates for the label
-        applyAttributes(label, {
-          x: i * xStep + xRange[0],
-          y: yRange[1] + font
-        })
-
-        // Set the styling for the label
-        applyAttributes(label, {
-          fill:
-            typeof color == "object" ? color[2] || color[1] || color[0] : color,
-          "font-size": font,
-          "text-anchor": "middle"
-        })
-
-        group.appendChild(label)
-      }
-
-      container.appendChild(group)
-    }
-
-    return container
+    return (x, y) => [
+      x * xRatio + (padding.length > 2 ? padding[3] : padding[1]),
+      dimensions[1] -
+        (y * yRatio + (padding.length > 2 ? padding[2] : padding[0]))
+    ]
   }
 
-  function yAxis(props) {
-    const {
-      xRange,
-      yRange,
-      color = "blue",
-      thickness = 1,
-      text = false,
-      amount = 1,
-      font,
-      minmax
-    } = props
+  function lengthAngle(a, b, end) {
+    const xLength = b[0] - a[0]
+    const yLength = b[1] - a[1]
 
-    const container = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "g"
-    )
+    return [
+      Math.sqrt(Math.pow(xLength, 2) + Math.pow(yLength, 2)) * 0.15,
+      Math.atan2(yLength, xLength) + (end ? Math.PI : 0)
+    ]
+  }
 
-    const verticalLine = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "line"
-    )
+  function controlPoints(data, index, end = false) {
+    const previous = data[index - 1] || data[index]
+    const next = data[index + 1] || data[index]
 
-    // Set the coordinates for the vertical line
-    applyAttributes(verticalLine, {
-      x1: xRange[0],
-      x2: xRange[0],
-      y1: yRange[0],
-      y2: yRange[1]
+    const [length, angle] = lengthAngle(previous, next, end)
+
+    return [
+      data[index][0] + Math.cos(angle) * length,
+      data[index][1] + Math.sin(angle) * length
+    ]
+  }
+
+  function calcPath(data) {
+    return data.reduce((acc, d, i) => {
+      const [x, y] = d
+
+      if (i === 0) {
+        return `${acc}M${x} ${y}`
+      } else {
+        const cpStart = controlPoints(data, i - 1)
+        const cpEnd = controlPoints(data, i, true)
+
+        return `${acc} C${cpStart[0]} ${cpStart[1]}, ${cpEnd[0]} ${
+          cpEnd[1]
+        }, ${x} ${y}`
+      }
+    }, "")
+  }
+  function createPath(_data, style) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path")
+
+    const data = _data.map(d => convert(...d)) // Convert the data into coordinates, based on the svg height and width
+
+    const attributes = style
+
+    attributes.d = calcPath(data)
+
+    applyAttributes(path, attributes)
+
+    return path
+  }
+
+  function createRect(dimensions, padding) {
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect")
+
+    applyAttributes(rect, {
+      stroke: "red",
+      "stroke-width": "1",
+      x: padding.length > 2 ? padding[3] : padding[1],
+      y: padding.length > 2 ? padding[0] : padding[0],
+      fill: "none",
+      width:
+        dimensions[0] -
+        (padding.length > 2 ? padding[1] + padding[3] : padding[1] * 2),
+      height:
+        dimensions[1] -
+        (padding.length > 2 ? padding[0] + padding[2] : padding[0] * 2)
     })
 
-    // Set the styling for the vertical line
-    applyAttributes(verticalLine, {
-      stroke: typeof color == "object" ? color[0] : color,
-      "stroke-width": typeof thickness == "object" ? thickness[0] : thickness
-    })
-
-    // Add vertical line to the container
-    container.appendChild(verticalLine)
-
-    // Create the horizontal lines
-    for (let i = 0; i <= amount - 1; i++) {
-      const group = document.createElementNS("http://www.w3.org/2000/svg", "g")
-
-      const yStep = (yRange[1] - yRange[0]) / (amount - 1)
-
-      if (i < amount - 1) {
-        const horizontalLine = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "line"
-        )
-
-        // Set the coordinates for the vertical line
-        applyAttributes(horizontalLine, {
-          y1: yRange[0] + i * yStep,
-          y2: yRange[0] + i * yStep,
-          x1: xRange[0],
-          x2: xRange[1]
-        })
-
-        // Set the styling for the vertical line
-        applyAttributes(horizontalLine, {
-          stroke: typeof color == "object" ? color[1] || color[0] : color,
-          "stroke-width":
-            typeof thickness == "object"
-              ? thickness[1] || thickness[0]
-              : thickness
-        })
-
-        // Add vertical line to the group
-        group.appendChild(horizontalLine)
-      }
-
-      // Add text if specified
-      if (text) {
-        const label = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "text"
-        )
-
-        if (text === true) {
-          // Text is true use i as text
-          const text = document.createTextNode(
-            Math.round(((minmax[1] - minmax[0]) / (amount - 1)) * i)
-          )
-
-          label.appendChild(text)
-        } else if (typeof text === "string") {
-          // Use the string as key for the data
-        }
-
-        // Set the coordinates for the label
-        applyAttributes(label, {
-          y: yRange[1] - i * yStep,
-          x: xRange[0] - font / 2
-        })
-
-        // Set the styling for the label
-        applyAttributes(label, {
-          fill:
-            typeof color == "object" ? color[2] || color[1] || color[0] : color,
-          "font-size": font,
-          "text-anchor": "end",
-          "alignment-baseline": "middle"
-        })
-
-        group.appendChild(label)
-      }
-
-      container.appendChild(group)
-    }
-
-    return container
+    graph.appendChild(rect)
   }
 
   const dimensions = svgDimensions(graph)
 
-  graph.appendChild(
-    xAxis({
-      xRange: [50, dimensions.width - 25],
-      yRange: [25, dimensions.height - 25],
-      color: ["grey", "grey", "black"],
-      thickness: [3, 1],
-      text: true,
-      font: 20,
-      data: data
-    })
-  )
+  function updatePath(dz) {
+    const paths = document.querySelector("#graph-1 path")
 
-  graph.appendChild(
-    yAxis({
-      xRange: [50, dimensions.width - 25],
-      yRange: [25, dimensions.height - 25],
-      color: ["grey", "grey", "black"],
-      thickness: [3, 1],
-      amount: 4,
-      text: true,
-      font: 15,
-      minmax: [Math.min(...data), roundNicely(Math.max(...data), 50)]
-    })
-  )
+    const convert2 = pointsConverter(dimensions, [10, 10], dz)
 
-  // <bold>oijef</bold>
+    const data = dz.map(d => convert2(...d))
 
-  function plotLine(props) {
-    const { xRange, yRange, color, fill = false, minmax, data } = props
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path")
-    const pathD = []
+    const d = calcPath(data)
 
-    const f = (yRange[1] - yRange[0]) / (minmax[1] - minmax[0])
-    const xStep = (xRange[1] - xRange[0]) / (data.length - 1)
-
-    for (let i = 0; i <= data.length - 1; i++) {
-      if (i === 0) {
-        pathD.push(`M${i * xStep + xRange[0]} ${yRange[1] - data[i]}`)
-      } else {
-        pathD.push(`L${i * xStep + xRange[0]} ${yRange[1] - f * data[i]}`)
-      }
-    }
-
-    if (fill) {
-      pathD.push(`V${yRange[1]} Z`)
-    }
-
-    applyAttributes(path, {
-      d: pathD.join(" "),
-      stroke: color,
-      fill: fill ? color : "none",
-      "stroke-width": 2
-    })
-
-    graph.appendChild(path)
+    paths.setAttribute("d", d)
   }
 
-  plotLine({
-    xRange: [50, dimensions.width - 25],
-    yRange: [25, dimensions.height - 25],
-    color: "blue",
-    fill: true,
-    minmax: [Math.min(...data), roundNicely(Math.max(...data), 50)],
-    data: data
-  })
+  function pathTransition(a1, a2, steps) {
+    const [startData, endData] = [[...a1], [...a2]]
+
+    const counterArray = endData.map((d, i) => (d[1] - startData[i][1]) / steps)
+
+    let counter = 0
+
+    updateTransition(startData)
+
+    function updateTransition(array) {
+      const outputData = array.map((d, i) => [d[0], d[1] + counterArray[i]])
+
+      setTimeout(() => {
+        counter++
+
+        if (counter === steps) {
+          updatePath(outputData.map(d => [d[0], Math.round(d[1])]))
+        } else {
+          updatePath(outputData)
+
+          return updateTransition(outputData)
+        }
+      }, 1000 / 60)
+    }
+  }
 })()
