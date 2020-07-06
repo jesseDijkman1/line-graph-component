54d0364653900c18ef476a161a392bca725a91e0
jesseDijkman1 <jesse.dijkman@hva.nl>
2020-07-06 23:56:44 +0200

    Implemented some simple easing

--- src/js/graph.js
+++ src/js/graph.js
@@ -11,6 +11,27 @@ import {
   times,
 } from "./utils"
 
+const EasingFunctions = {
+  // no easing, no acceleration
+  linear: (t) => t,
+  // accelerating from zero velocity
+  easeInQuad: (t) => t * t,
+  // decelerating to zero velocity
+  easeOutQuad: (t) => t * (2 - t),
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  // accelerating from zero velocity
+  easeInCubic: (t) => t * t * t,
+  // decelerating to zero velocity
+  easeOutCubic: (t) => --t * t * t + 1,
+  // acceleration until halfway, then deceleration
+  easeInOutCubic: (t) =>
+    t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+  // accelerating from zero velocity
+  easeInQuart: (t) => t * t * t * t,
+  // acceleration until halfway,
+}
+
 class Graph {
   static defaultOptions() {
     return {
@@ -131,13 +152,14 @@ class Graph {
 
     let steps = 0
 
+    const normalize = normalizer(0, 60)
+
     const oldVectors = this.currentVectors
     const newVectors = this.transformVectors(vectorsArray)
 
     const differentialVectors = newVectors.map((vector, index) => {
       const { x, y } = difference(vector, oldVectors[index])
-
-      return { x: x / _steps, y: y / _steps }
+      return { x, y }
     })
 
     function frames(...args) {
@@ -150,7 +172,10 @@ class Graph {
       const transitionVectors = oldVectors.map((oldVector, index) => {
         const { x, y } = plus(
           oldVector,
-          times(differentialVectors[index], steps)
+          times(
+            differentialVectors[index],
+            EasingFunctions.easeInOutCubic(normalize(steps))
+          )
         )
 
         return { x, y }
