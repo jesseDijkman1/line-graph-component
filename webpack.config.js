f89d3407a84a9b7d8b7a7f5411c0955cd8b66e15
jesseDijkman1 <jesse.dijkman@hva.nl>
2020-07-06 18:38:47 +0200

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+}
